e1fba320b8955493c4bed0a68af352eb73ade085
David Wnek <davidwnek@gmail.com>
2018-02-01 21:28:02 -0600

    Added Pages & PagesQuestions Services

---
+++ src/SurveyMonkey/Service/PagesQuestionsService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace davidwnek\SurveyMonkey\Service;
+
+use davidwnek\SurveyMonkey\HTTPMethod;
+use davidwnek\SurveyMonkey\Model\Survey;
+use davidwnek\SurveyMonkey\Response\ListResponse;
+
+class PagesQuestionsService extends ClientService
+{
+    const RESULTS_PER_PAGE = 25;
+
+    /**
+     * @param int $survey
+     * @param int $question
+     * @param int $page
+     * @param int $resultsPerPage
+     * @param string $sortBy
+     * @param string $sortOrder
+     * @return ListResponse
+     */
+    public function getSurveyPagesQuestions($survey, $question, $page = 1, $resultsPerPage = self::RESULTS_PER_PAGE, $sortBy = 'title', $sortOrder = 'DESC')
+    {
+        $params = array(
+            'page' => $page,
+            'per_page' => $resultsPerPage,
+            'sort_by' => $sortBy,
+            'sort_order' => $sortOrder,
+        );
+
+        $response = $this->client->run(sprintf('/surveys/%s/pages/%s/questions', $survey, $question), HTTPMethod::GET, $params);
+
+        if($response->isError()) {
+            return $response;
+        }
+
+        return new ListResponse($response->getResponse(), $this->getClient(), Survey::class);
+    }
+
+    /**
+     * @param array $params
+     * @param string $name
+     * @param mixed $value
+     * @param null $function
+     * @param null $parameter
+     */
+    private function appendQueryParameter(&$params, $name, $value, $function = null, $parameter = null)
+    {
+        if(empty($value)) {
+            return;
+        }
+
+        if($function !== null) {
+            $params[$name] = $value->$function($parameter);
+            return;
+        }
+        $params[$name] = $value;
+    }
+}
