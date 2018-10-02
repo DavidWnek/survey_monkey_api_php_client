66998c9b9024d6e5259695730d943c9710f59c98
David Wnek <davidwnek@gmail.com>
2018-10-02 10:21:33 -0500

    Added responses and response endpoints

--- src/SurveyMonkey/Service/SurveyResponseService.php
+++ src/SurveyMonkey/Service/SurveyResponseService.php
@@ -7,6 +7,28 @@ use davidwnek\SurveyMonkey\Model\SurveyResponse;
 
 class SurveyResponseService extends ClientService
 {
+    public function getResponses($survey)
+    {
+        $response = $this->client->run(sprintf('/surveys/%s/responses', $survey), HTTPMethod::GET);
+
+        if($response->isError()) {
+            return $response;
+        }
+
+        return SurveyResponse::createFromString($this->getClient(), $response->getResponse()->getBody()->__toString());
+    }
+
+    public function getResponse($survey, $response)
+    {
+        $response = $this->client->run(sprintf('/surveys/%s/responses/%s', $survey, $response), HTTPMethod::GET);
+
+        if($response->isError()) {
+            return $response;
+        }
+
+        return SurveyResponse::createFromString($this->getClient(), $response->getResponse()->getBody()->__toString());
+    }
+
     public function getResponseDetails($survey, $response)
     {
         $response = $this->client->run(sprintf('/surveys/%s/responses/%s/details', $survey, $response), HTTPMethod::GET);
